ddf39c3dafe9b562f44ddfcc90146d948bc2cfba
Egor <aldes@list.ru>
2022-12-16 23:09:49 +0300

    1 Dz

---
+++ 1_Dz/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 1: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Не использовать массив.
+
+M=5 ; 0, 7, 8, -2, -2 -> 2
+M=5 ; -1, -7, 567, 89, 223 -> 3
+*/
+
+Console.WriteLine("введите М");
+int M = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 0; M > i; i++)
+{
+    Console.WriteLine("введите число");
+    int X = Convert.ToInt32(Console.ReadLine());
+    if(X>0)
+    sum = sum + 1;
+}
+
+Console.WriteLine("кол-во чисел больше нуля = " + sum);
